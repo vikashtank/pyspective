2637ef4c6a1f648ce221ee0da6860bf1f5109c61
Vikash Tank <vikashtank@hotmail.co.uk>
2018-07-28 14:24:57 +0100

    remove unecessary functions

--- tracker/tools/wrappers.py
+++ tracker/tools/wrappers.py
@@ -1,17 +1,5 @@
 
 
-def print_wrapper(function):
-    """
-    Decorating function that wraps a function and prints its arguments before executing
-    """
-    def wrapper(*args, **kwargs):
-        print("name", function.__name__)
-        print("args", args)
-        print("kwargs", kwargs)
-        return function(*args, **kwargs)
-    return wrapper
-
-
 def print_args(function, *args, **kwargs):
         print("function", function.__name__)
         print("args", args)
@@ -27,11 +15,3 @@ def before_call(before_call_function):
         return inner_wrapper
 
     return wrapper
-
-
-@before_call(print_args)
-def test_function(a, b):
-    return a + b
-
-if __name__ == '__main__':
-    test_function(5, 6)
